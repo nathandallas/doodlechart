9d63b5ed96bb3a51dafdbeca65db7cd4ba17a201
Nathan <87546582+nathandallas@users.noreply.github.com>
2026-08-22 03:26:51 -0400

    added row/column numbers to chart

--- src/components/ChartCanvas.vue
+++ src/components/ChartCanvas.vue
@@ -3,7 +3,6 @@ import { ref, computed } from 'vue'
 import { stitchPoints, stitchViewSize, stitchAtPoint } from '../engine/geometry.js'
 import { stitchDimensionsForGauge } from '../engine/gauge.js'
 
-
 const props = defineProps({
   chart: { type: Object, required: true },
   mode: { type: String, default: 'grid' },
@@ -22,6 +21,13 @@ const stitchGeom = computed(() => ({
 }))
 
 const cellSize = computed(() => ({ width: dims.value.W, height: dims.value.T }))
+
+// Grid numbers
+const colNum = (c) => props.chart.cols - c
+const rowNum = (r) => props.chart.rows - r
+
+const colIndex = computed(() => Array.from({ length: props.chart.cols }, (_, i) => i))
+const rowIndex = computed(() => Array.from({ length: props.chart.rows }, (_, i) => i))
 
 const viewSize = computed(() =>
   props.mode === 'chevron'
@@ -64,40 +70,136 @@ function handlePointerUp() {
 </script>
 
 <template>
-  <svg
-    ref="svgEl"
-    :width="viewSize.width"
-    :height="viewSize.height"
-    @pointerdown="handlePointerDown"
-    @pointermove="handlePointerMove"
-    @pointerup="handlePointerUp"
-  >
-    <template v-if="mode === 'chevron'">
-      <template v-for="(row, r) in chart.cells" :key="r">
-        <polygon
-          v-for="(colorIndex, c) in row"
-          :key="c"
-          :points="stitchPoints(c, r, stitchGeom)"
-          :fill="chart.palette[colorIndex]"
-          :stroke="chart.gridColor"
-          :stroke-opacity="chart.gridOpacity"
-        />
+  <div class="chart-grid-wrapper">
+    <div class="chart-labels chart-labels-top" :style="{ width: viewSize.width + 'px' }">
+      <span
+        v-for="c in colIndex"
+        :key="c"
+        class="chart-label"
+        :style="{ left: c * cellSize.width + cellSize.width / 2 + 'px' }"
+        >{{ colNum(c) }}</span
+      >
+    </div>
+
+    <div class="chart-labels chart-labels-left" :style="{ height: viewSize.height + 'px' }">
+      <template v-for="r in rowIndex" :key="r">
+        <span
+          v-if="rowNum(r) % 2 === 0"
+          class="chart-label"
+          :style="{ top: r * cellSize.height + cellSize.height / 2 + 'px' }"
+          >{{ rowNum(r) }}</span
+        >
       </template>
-    </template>
-    <template v-else>
-      <template v-for="(row, r) in chart.cells" :key="r">
-        <rect
-          v-for="(colorIndex, c) in row"
-          :key="c"
-          :x="c * cellSize.width"
-          :y="r * cellSize.height"
-          :width="cellSize.width"
-          :height="cellSize.height"
-          :fill="chart.palette[colorIndex]"
-          :stroke="chart.gridColor"
-          :stroke-opacity="chart.gridOpacity"
-        />
+    </div>
+    <svg
+      ref="svgEl"
+      :width="viewSize.width"
+      :height="viewSize.height"
+      @pointerdown="handlePointerDown"
+      @pointermove="handlePointerMove"
+      @pointerup="handlePointerUp"
+    >
+      <template v-if="mode === 'chevron'">
+        <template v-for="(row, r) in chart.cells" :key="r">
+          <polygon
+            v-for="(colorIndex, c) in row"
+            :key="c"
+            :points="stitchPoints(c, r, stitchGeom)"
+            :fill="chart.palette[colorIndex]"
+            :stroke="chart.gridColor"
+            :stroke-opacity="chart.gridOpacity"
+          />
+        </template>
       </template>
-    </template>
-  </svg>
+      <template v-else>
+        <template v-for="(row, r) in chart.cells" :key="r">
+          <rect
+            v-for="(colorIndex, c) in row"
+            :key="c"
+            :x="c * cellSize.width"
+            :y="r * cellSize.height"
+            :width="cellSize.width"
+            :height="cellSize.height"
+            :fill="chart.palette[colorIndex]"
+            :stroke="chart.gridColor"
+            :stroke-opacity="chart.gridOpacity"
+          />
+        </template>
+      </template>
+    </svg>
+
+    <div class="chart-labels chart-labels-right" :style="{ height: viewSize.height + 'px' }">
+      <template v-for="r in rowIndex" :key="r">
+        <span
+          v-if="rowNum(r) % 2 === 1"
+          class="chart-label"
+          :style="{ top: r * cellSize.height + cellSize.height / 2 + 'px' }"
+          >{{ rowNum(r) }}</span
+        >
+      </template>
+    </div>
+
+    <div class="chart-labels chart-labels-bottom" :style="{ width: viewSize.width + 'px' }">
+      <span
+        v-for="c in colIndex"
+        :key="c"
+        class="chart-label"
+        :style="{ left: c * cellSize.width + cellSize.width / 2 + 'px' }"
+        >{{ colNum(c) }}</span
+      >
+    </div>
+  </div>
 </template>
+
+<style scoped>
+.chart-grid-wrapper {
+  display: grid;
+  grid-template-columns: auto auto auto;
+  grid-template-rows: auto auto auto;
+  grid-template-areas:
+    '.    top    .'
+    'left svg    right'
+    '.    bottom .';
+  width: fit-content;
+}
+
+.chart-grid-wrapper > svg {
+  grid-area: svg;
+}
+
+.chart-labels {
+  position: relative;
+}
+.chart-labels-top {
+  grid-area: top;
+  height: 1.4em;
+}
+.chart-labels-bottom {
+  grid-area: bottom;
+  height: 1.4em;
+}
+.chart-labels-left {
+  grid-area: left;
+  width: 1.6em;
+}
+.chart-labels-right {
+  grid-area: right;
+  width: 1.6em;
+}
+
+.chart-label {
+  position: absolute;
+  transform: translate(-50%, -50%);
+  font-size: 0.85rem;
+  color: var(--text-primary);
+  white-space: nowrap;
+}
+.chart-labels-top .chart-label,
+.chart-labels-bottom .chart-label {
+  top: 50%;
+}
+.chart-labels-left .chart-label,
+.chart-labels-right .chart-label {
+  left: 50%;
+}
+</style>
