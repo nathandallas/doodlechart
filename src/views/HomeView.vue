ecae97b0e4a0e7027b43923a4c62cead1086696c
Nathan <87546582+nathandallas@users.noreply.github.com>
2026-09-16 23:42:29 -0400

    added better mobile response to nav and home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -61,6 +61,7 @@ function onSetupConfirm() {
   background-size: 30px 20px;
   -webkit-mask-image: radial-gradient(ellipse 70% 60% at 50% 0%, #000 60%, transparent 100%);
   mask-image: radial-gradient(ellipse 70% 60% at 50% 0%, #000 60%, transparent 100%);
+  transition: opacity 0.3s ease;
 }
 
 .page-footer {
@@ -76,14 +77,20 @@ function onSetupConfirm() {
   border-radius: 20px;
   z-index: 4;
   background-color: var(--secondary60);
+  transition:
+    max-width 0.2s ease,
+    margin 0.2s ease,
+    padding 0.2s ease;
 }
 
 h2 {
   font-size: 4rem;
+  transition: font-size 0.2s ease;
 }
 
 h3 {
   font-size: 1.5rem;
+  transition: font-size 0.2s ease;
 }
 
 .chart-selection {
@@ -92,11 +99,56 @@ h3 {
   display: flex;
   justify-content: space-around;
   gap: 2rem;
+  transition: gap 0.2s ease;
 }
 
 button {
   font-size: 1.5rem;
   padding: 1rem 1.5rem;
   border-radius: 10px;
+  transition: all 0.2s ease;
+}
+
+@media (min-width: 641px) and (max-width: 1024px) {
+  .cta {
+    max-width: 600px;
+  }
+
+  h2 {
+    font-size: 3rem;
+  }
+
+  h3 {
+    font-size: 1.3rem;
+  }
+}
+
+@media (max-width: 640px) {
+  .cta {
+    max-width: 100%;
+    margin: 4rem 2rem 2rem;
+    padding: 0 18px;
+  }
+
+  .grid-background {
+    opacity: 0.5;
+  }
+
+  h2 {
+    font-size: 3rem;
+  }
+
+  h3 {
+    font-size: 1.1rem;
+  }
+
+  .chart-selection {
+    flex-direction: column;
+    gap: 1rem;
+  }
+
+  .chart-selection button {
+    width: 100%;
+  }
 }
 </style>
